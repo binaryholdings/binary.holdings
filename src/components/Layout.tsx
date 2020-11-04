308c237107380e061fdc9b0bff5f97b53d55483b
wpank <9498646+wpank@users.noreply.github.com>
2020-11-04 15:26:43 +0100

    remove boilerplate

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,8 +12,8 @@ import { store } from "../store";
 
 export const menuItems = [
   { name: "Home", path: "/", exact: true, icon: "home", inverted: true },
-  { name: "About", path: "/about/", exact: true, icon: "info circle" },
-  { name: "Blog", path: "/blog/", exact: false, icon: "newspaper" },
+  // { name: "About", path: "/about/", exact: true, icon: "info circle" },
+  // { name: "Blog", path: "/blog/", exact: false, icon: "newspaper" },
 ];
 
 export interface LayoutProps {
@@ -49,7 +49,6 @@ const Layout = (props: LayoutProps) => {
           {/* Footer */}
           <Segment inverted vertical style={{ position: "absolute", bottom: 0, width: "100%" }}>
             <Container textAlign="center">
-              <p>Powered with <Icon name="heart" /> by Gatsby 2.0</p>
             </Container>
           </Segment>
         </Sidebar.Pusher>
