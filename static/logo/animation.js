815ba53113d227291958296147b45e27bf7a0728
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-11-07 18:48:48 +0100

    binary dissolve animation

--- static/logo/animation.js
+++ static/logo/animation.js
@@ -1,3 +1,8 @@
+// Configuration for animation
+const totalTime = 5_000;
+const animationTime = 200;
+const chance = .05;
+
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -14,11 +19,6 @@ async function binaryAnimation(repeat = true) {
 	for (let i = 0; i < length; i++) {
 		tracker.push(false);
 	}
-
-	// Configuration for animation
-	let totalTime = 5_000;
-	let animationTime = 200;
-	let chance = .05;
 
 	for (let i = 0; i < totalTime; i += animationTime) {
 		// The string we will construct.
@@ -54,6 +54,37 @@ async function binaryAnimation(repeat = true) {
 
 	if (repeat) {
 		await sleep(totalTime);
+		await binaryDissolve(repeat);
+	}
+}
+
+async function binaryDissolve(repeat = true) {
+	let title = document.querySelector(".binary")
+
+	// Start with the inner text
+	let finalTitle = title.innerText.split("");
+	let length = finalTitle.length;
+
+	let extraCycles = 5;
+
+	for (let i = 0; i < length + extraCycles; i ++) {
+		// The string we will construct.
+		let tempString = [];
+		for (let j = 0; j < length; j++) {
+			if (j <= i && finalTitle[j] != " ") {
+				let random = Math.random();
+				let zeroOrOne = Math.round(random);
+				tempString.push(zeroOrOne);
+			} else {
+				tempString.push(finalTitle[j])
+			}
+		}
+
+		title.innerText = tempString.join("");
+		await sleep(animationTime);
+	}
+
+	if (repeat) {
 		await binaryAnimation(repeat);
 	}
 }
